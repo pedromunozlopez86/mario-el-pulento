4a37c339a30665dedfceed6c9ce04701b7ceb848
Pedro Muñoz L <74686288+pedromunozlopez86@users.noreply.github.com>
2023-11-03 17:40:35 -0300

    Archivos iniciales del proyecto de MakeCode

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Mario":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 8 8 8 8 8 8 8 8 8 8 8 . 
+. . . . 8 8 8 8 8 8 8 8 8 8 8 . 
+1 . . . . 1 8 . . 8 8 8 8 8 8 . 
+1 . . . . 1 . . . 8 8 8 8 8 8 . 
+. 8 . . . . . 8 8 8 8 8 8 8 8 . 
+. . . . 1 1 8 . 8 8 8 8 8 8 8 . 
+. . . 8 8 8 8 8 . 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 . 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 . . 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
